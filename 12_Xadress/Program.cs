d9ebaf72f9f777d287ebc529fe677b753038179a
Roberto Buerger <roberto.buerger@gmail.com>
2020-01-24 13:18:15 -0300

    Acrescentada uma mensagem de Bom Dia

--- 12_Xadress/Program.cs
+++ 12_Xadress/Program.cs
@@ -6,7 +6,7 @@ namespace _12_Xadress
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
